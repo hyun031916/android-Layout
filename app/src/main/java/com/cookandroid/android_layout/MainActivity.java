523627f2f7cf8aeaafc2b6d8e2e089c3265fbc9c
ksh <hyun031916@gmail.com>
2020-09-09 01:38:49 +0900

    #2 gravity와 layout_gravity 속성 (left/center/right

--- app/src/main/java/com/cookandroid/android_layout/MainActivity.java
+++ app/src/main/java/com/cookandroid/android_layout/MainActivity.java
@@ -16,9 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b1 = (Button)findViewById(R.id.b1);
-        TextView t1 = (TextView)findViewById(R.id.tv1);
-        CheckBox cb1 = (CheckBox) findViewById(R.id.cb1);
-        RadioButton rb1 = (RadioButton)findViewById(R.id.rb1);
-        Switch s1 = (Switch)findViewById(R.id.s1);
+//        TextView t1 = (TextView)findViewById(R.id.tv1);
+//        CheckBox cb1 = (CheckBox) findViewById(R.id.cb1);
+//        RadioButton rb1 = (RadioButton)findViewById(R.id.rb1);
+//        Switch s1 = (Switch)findViewById(R.id.s1);
+        Button b2 = (Button)findViewById(R.id.b2);
+        Button b3 = (Button)findViewById(R.id.b3);
     }
 }
