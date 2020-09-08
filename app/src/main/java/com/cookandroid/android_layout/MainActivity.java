5ade69767d162e7da632db0a7a6f2f4acdecec07
ksh <hyun031916@gmail.com>
2020-09-09 01:30:49 +0900

    #1 orientation 속성 XML 코드 horizontal/vertical

--- app/src/main/java/com/cookandroid/android_layout/MainActivity.java
+++ app/src/main/java/com/cookandroid/android_layout/MainActivity.java
@@ -3,6 +3,11 @@ package com.cookandroid.android_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = (Button)findViewById(R.id.b1);
+        TextView t1 = (TextView)findViewById(R.id.tv1);
+        CheckBox cb1 = (CheckBox) findViewById(R.id.cb1);
+        RadioButton rb1 = (RadioButton)findViewById(R.id.rb1);
+        Switch s1 = (Switch)findViewById(R.id.s1);
     }
 }
